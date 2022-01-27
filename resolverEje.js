4b2fd1c254cd2c0b38ff65841b25e0cfa6ffab68
Leonardo Rojas Quevedo <leonardorojasq@outlook.com>
2022-01-26 21:31:55 -0500

    Ejercicios

---
+++ resolverEje.js
@@ -0,0 +1,38 @@
+/* 1) Programa una función que cuente el número de caracteres de una cadena
+de texto, pe. miFunción("Hola Mundo") devolverá 10 */
+      //Tu codigo va Aqui
+
+/*       function miFuncion(palabra){
+               return palabra.length;
+            }
+       console.log(miFuncion(c));*/
+
+/* 2) Programa una función que te devuelva el texto recortado según el número de
+caracteres indicados, pe. miFunción("Hola Mundo", 4) devolverá "Hola" */
+      //Tu codigo va Aqui
+      /*let texto = "Hola Mundo";
+      function recorte(){
+        return texto.substring(4, 0);
+      }
+      console.log(recorte());*/
+
+/* 3) Programa una función que dada una String te devuelva un Array de textos separados
+por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'] */
+      //Tu codigo va Aqui
+     /* function devolverArray(){
+          let texto = "hola que tal";
+          let textoSeparado = texto.split(" ");
+          return textoSeparado ;
+      }
+console.log(devolverArray());*/
+
+/* 4) Programa una función que repita un texto X veces,
+pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo */
+      //Tu codigo va Aqui
+      function repe(){
+        let texto = "Hola Mundo ";
+        let palabra = texto.repeat(3);
+        return palabra;
+      }
+console.log(repe());
+
