45c00d45ae6a47bdca8dc090dbcc7672c825e1b6
Leonardo Rojas Quevedo <leonardorojasq@outlook.com>
2022-02-02 13:52:24 -0500

    tareas02

--- resolverEje.js
+++ resolverEje.js
@@ -516,9 +516,57 @@
 
 /* 21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, 
     pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25] */
+    const elevarCuadrado = (numeros = undefined) => {
+      if (numeros === undefined) return console.log("Debes Ingresar un array de numeros");
+    
+      if (numeros.length === 0) return console.log("El array ingresado está Vacio");
+    
+      if (!(numeros instanceof Array)) return console.log(`El valor ${numeros} no es un Array de Numeros`);
+    
+      for (let num of numeros) {
+       if(typeof num !== "number") return console.log(`El valor ${num} ingresado no es valido`)
+      }
+        const newNumeros = numeros.map(el => el * el)
+
+      return console.log(`El arreglo original es ${numeros},\n Areglo elevado al cuadrado : ${newNumeros}`);
+    }
+    // elevarCuadrado();
+    // elevarCuadrado("hola");
+    // elevarCuadrado(345);
+    // elevarCuadrado([1, 2, 3]);
+    // elevarCuadrado([1, 4, 5]);
+    // elevarCuadrado([1,  4, 8]);
+
+    
+
+
+
 
 /* 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. 
     miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60] */
+      const DevolverNumeroMax = (numeros = undefined) =>{
+      if (numeros === undefined) return console.log("Debes Ingresar un array de numeros");
+    
+      if (numeros.length === 0) return console.log("El array ingresado está Vacio");
+    
+      if (!(numeros instanceof Array)) return console.log(`El valor ${numeros} no es un Array de Numeros`);
+    
+      for (let num of numeros) {
+       if(typeof num !== "number") return console.log(`El valor ${num} ingresado no es valido`)
+      }
+
+      return console.info(`Arreglo original ${numeros}\nValor mayor : ${Math.max(...numeros)},\n
+      Valor menor:${Math.min(...numeros)}`) 
+        
+      }
+      DevolverNumeroMax()
+      DevolverNumeroMax(false)
+      DevolverNumeroMax([])
+      DevolverNumeroMax([2,3,89,58])
+
+
+
+
 
 /* 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero 
     almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]} */
@@ -547,3 +595,22 @@
 //       - Crea un método que devuelva toda la ficha técnica de la película.
 //       - Apartir de un arreglo con la información de 3 películas genera 3 instancias de la clase de forma automatizada e imprime la ficha técnica de cada película.
 //      Géneros Aceptados: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary ,Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western.
+
+
+// clonado
+// var list = ["A",1,2,3,5,8,13];
+ 
+// //clonamos el array original (shallow copy)
+// var cloned = list.slice(0);
+ 
+// //modificamos el primer elemento del array original
+// list[0] = 9;
+ 
+// //agregamos un elemento al inicio del array original
+// list.unshift("X");
+ 
+// //agregamos un elemento al final del array clonado
+// cloned.push(21);
+ 
+// console.log("original: ", list);
+// console.log("cloned: ", cloned);
